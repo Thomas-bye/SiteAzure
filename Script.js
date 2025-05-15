d3fe113da9b290843bcec8fc0f10421abf20911d
Thomas-bye <s01301a@upla.edu.pe>
2025-05-14 22:52:34 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -8,8 +8,8 @@ document.addEventListener("DOMContentLoaded", function() {
 
         sesionDiv.innerHTML = `
             <h2>Sesión ${i}</h2>
-            <img src="imagen${i}.jpg" alt="Imagen de la sesión ${i}">
-            <a href="trabajo${i}.html" class="boton">Visualizar</a>
+            <img src="img/Parcial_${i}.png" alt="Imagen de la sesión ${i}">
+            <a href="tareas/pa${i}." class="boton">Visualizar</a>
             <a href="documento${i}.pdf" download class="boton">Descargar PDF</a>
         `;
 
