965167a8a1f6f9edfe69d185b27bd6d313002d2e
Thomas-bye <s01301a@upla.edu.pe>
2025-05-14 22:37:23 -0600

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,1 +1,18 @@
+// script.js
+document.addEventListener("DOMContentLoaded", function() {
+    const sesionesContainer = document.querySelector(".sesiones");
 
+    for (let i = 1; i <= 6; i++) {
+        let sesionDiv = document.createElement("div");
+        sesionDiv.classList.add("sesion");
+
+        sesionDiv.innerHTML = `
+            <h2>Sesión ${i}</h2>
+            <img src="imagen${i}.jpg" alt="Imagen de la sesión ${i}">
+            <a href="trabajo${i}.html" class="boton">Visualizar</a>
+            <a href="documento${i}.pdf" download class="boton">Descargar PDF</a>
+        `;
+
+        sesionesContainer.appendChild(sesionDiv);
+    }
+});
